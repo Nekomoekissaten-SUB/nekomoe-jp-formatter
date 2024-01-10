8ed725786a6820243a157c05d44fcb1efd6ec0a0
Ronny-zzl <zzlronny@gmail.com>
2024-01-10 18:37:47 +0900

    去除分行处理模式

--- dist/worker/replace.js
+++ dist/worker/replace.js
@@ -1,41 +1,21 @@
 self.onmessage = function handleMessageFromMain(msg) {
   const { rules, input } = msg.data;
-  // 删除ass文件头
-  let processedInput = input.replace(
-    /^\[Script Info\][\s\S]*?Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text\n/,
-    ""
-  );
-  let lineList = processedInput.split("\n");
   let ruleList = rules.split("\n");
-  const length = lineList.length;
-  let process = 0;
 
-  let res = lineList.map((line, index) => {
-    let newLine = line;
-    if (!newLine) return newLine;
-    if (!newLine.trim()) return newLine;
-    ruleList.forEach((rule) => {
-      if (!rule) return;
-      if (!rule.trim()) return;
-      let [prefix, pattern, replacement] = rule.split("\t");
-      if (prefix !== "on") return;
-      const reg = new RegExp(pattern, "g");
-      replacement = replacement.replaceAll(/\\(\d+)/g, "$$1");
+  let newInput = input;
+  ruleList.forEach((rule) => {
+    if (!rule) return;
+    if (!rule.trim()) return;
+    // 如果规则以#开头，忽略此行
+    if (rule.startsWith("#")) return;
+    let [prefix, pattern, replacement, description] = rule.split("\t");
+    console.log(description);
+    if (prefix !== "on") return;
+    const reg = new RegExp(pattern, "g");
+    replacement = replacement.replaceAll(/\\(\d+)/g, (match, p1) => "$" + p1);
 
-      newLine = newLine.replaceAll(reg, replacement);
-    });
-    // 行数超过 500 时显示进度
-    if (length > 500) {
-      let currentProcess = Math.floor((index / length) * 100);
-      if (currentProcess !== process) {
-        process = currentProcess;
-        self.postMessage({ status: "process", data: process });
-      }
-    }
-    return newLine;
+    newInput = newInput.replaceAll(reg, replacement);
   });
 
-  process = 0;
-  res = res.join("\n");
-  self.postMessage({ status: "success", data: res });
+  self.postMessage({ status: "success", data: newInput });
 };
