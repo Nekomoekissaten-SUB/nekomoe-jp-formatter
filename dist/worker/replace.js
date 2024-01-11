1cd79745c2d85f8d194df2eca57304282b058371
Ronny-zzl <zzlronny@gmail.com>
2024-01-11 22:10:08 +0900

    初步完善tsv规则

--- dist/worker/replace.js
+++ dist/worker/replace.js
@@ -9,22 +9,23 @@ self.onmessage = function handleMessageFromMain(msg) {
     // 如果规则以#开头，忽略此行
     if (rule.startsWith("#")) return;
     let [prefix, pattern, replacement, description] = rule.split("\t");
-    if (description) {
-      console.log(description);
-      if (description.trim() == "删除残留外字") {
-        //寻找出newInput中所有符合“\[外:.+?\]”的字符串输出在console中
-        let reg = new RegExp("\\[外:.+?\\]", "g");
-        let result = newInput.match(reg);
-        console.log("未处理外字：" + result);
-      }
-    }
     if (prefix !== "on") return;
     const reg = new RegExp(pattern, "g");
-    replacement = replacement.replaceAll(/\\(\d+)/g, (match, p1) => "$" + p1);
+    replacement = replacement
+      .replaceAll(/\\(\d+)/g, (match, p1) => "$" + p1)
+      .replace(/\\n/g, "\n");
+    if (description) {
+      console.log(description);
+      if (description.trim() === "删除残留外字") {
+        //寻找出newInput中所有符合“\[外:.+?\]”的字符串输出在console中
+        let result = newInput.match(reg);
+        if (result) console.log("未处理外字：" + result);
+      }
+    }
 
     if (replacement.trim() == "toHalfWidth") {
       function toHalfWidth(str) {
-        console.log(str);
+        console.log("转换全角：" + str);
         return str
           .replace(/[Ａ-Ｚａ-ｚ０-９]/g, function (s) {
             return String.fromCharCode(s.charCodeAt(0) - 65248);
@@ -32,7 +33,8 @@ self.onmessage = function handleMessageFromMain(msg) {
           .replace(/’/g, "'")
           .replace(/　/g, " ")
           .replace(/，/g, ",")
-          .replace(/．/g, ".");
+          .replace(/．/g, ".")
+          .replace(/／/g, "/");
       }
       newInput = newInput.replace(reg, toHalfWidth);
       return;
